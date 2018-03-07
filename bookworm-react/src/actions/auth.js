49cfd055b30a45b2d9fef3d142b7398905b15119
Mattemyo <matias97marti@gmail.com>
2018-03-07 22:49:45 +0100

    login/logout with jwt tokens

--- bookworm-react/src/actions/auth.js
+++ bookworm-react/src/actions/auth.js
@@ -1,4 +1,4 @@
-import { USER_LOGGED_IN } from '../types';
+import { USER_LOGGED_IN, USER_LOGGED_OUT } from '../types';
 import api from '../api';
 
 export const userLoggedIn = (user: {}): {} => ({
@@ -6,7 +6,17 @@ export const userLoggedIn = (user: {}): {} => ({
   user
 });
 
+export const userLoggedOut = (): {} => ({
+  type: USER_LOGGED_OUT,
+  user
+});
+
 export const login = (credentials: {}): any => (dispatch: {}): any =>
-  api.user
-    .login(credentials)
-    .then((user: {}): any => dispatch(userLoggedIn(user)));
+  api.user.login(credentials).then((user: {}): any => {
+    localStorage.bookwormJWT = user.token;
+    dispatch(userLoggedIn(user));
+  });
+export const logout = (credentials: {}): any => (dispatch: {}): any => {
+  localStorage.removeItem = 'bookwormJWT';
+  dispatch(userLoggedOut());
+};
--- bookworm-react/src/components/pages/HomePage.js
+++ bookworm-react/src/components/pages/HomePage.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../../actions/auth';
 
-export default () => (
+const HomePage = ({
+  isAuthenticated,
+  logout
+}: {
+  isAuthenticated: boolean,
+  logout: {}
+}): Element<any> => (
   <div>
     <h1>Home Page</h1>
-    <Link to="/login">Login</Link>
+    {isAuthenticated ? (
+      <button onClick={(): any => logout()}>Logout</button>
+    ) : (
+      <Link to="/login">Login</Link>
+    )}
   </div>
 );
+
+function mapStateToProps(state: {}): {} {
+  return {
+    isAuthenticated: Boolean(state.user.token)
+  };
+}
+export default connect(mapStateToProps, { logout: actions.logout })(HomePage);
