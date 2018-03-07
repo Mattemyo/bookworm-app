65343ea631341f37f4a299c95125602f61dc8c02
Mattemyo <matias97marti@gmail.com>
2018-03-07 19:41:22 +0100

    creat server and add error message to client

--- bookworm-react/src/App.js
+++ bookworm-react/src/App.js
@@ -3,10 +3,10 @@ import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
 
-const App = () => (
+const App = (): Element<any> => (
   <div className="ui container">
     <Route path="/" exact component={HomePage} />
-    <Route path="/login" exact component={LoginPage} />;
+    <Route path="/login" exact component={LoginPage} />
   </div>
 );
 export default App;
--- bookworm-react/src/components/forms/LoginForm.js
+++ bookworm-react/src/components/forms/LoginForm.js
@@ -1,8 +1,7 @@
-  /*eslint no-undef: "error"*/
-import React, { Component, SyntheticEvent } from 'react';
-import { Form, Button } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Form, Button, Message } from 'semantic-ui-react';
 import Validator from 'validator';
-import InlineError from '../message/InlineError';
+import InlineError from '../messages/InlineError';
 
 export default class LoginForm extends Component<Props> {
   state = {
@@ -14,7 +13,6 @@ export default class LoginForm extends Component<Props> {
     errors: {}
   };
 
-
   onChange = ({ target }: {}) => {
     this.setState({
       data: { ...this.state.data, [target.name]: target.value }
@@ -24,23 +22,33 @@ export default class LoginForm extends Component<Props> {
   onSubmit = () => {
     const errors = this.validate(this.state.data);
     this.setState({ errors });
-    if(Object.keys(errors).length === 0) {
-      this.props.submit(this.state.data);
+    if (Object.keys(errors).length === 0) {
+      this.props
+        .submit(this.state.data)
+        .catch((err: {}): {} =>
+          this.setState({ errors: err.response.data.errors })
+        );
     }
   };
 
-  validate = (data: {}): {} => {
+  validate = (data: {}): any => {
     const errors = {};
     if (!Validator.isEmail(data.email)) errors.email = 'Invalid email';
     if (!data.password) errors.password = "Can't be blank";
     return errors;
   };
 
-  
-    render(): ?React$Element<any> {
-    const { data: { email, password }, errors } = this.state;
+  render(): Element<any> {
+    const { data, errors } = this.state;
+    console.log(errors);
     return (
       <Form onSubmit={this.onSubmit}>
+        {errors.global && (
+          <Message negative>
+            <Message.Header>Something went wrong</Message.Header>
+            <p>{errors.global}</p>
+          </Message>
+        )}
         <Form.Field error={Boolean(errors.email)}>
           <label htmlFor="email">Email</label>
           <input
@@ -48,10 +56,10 @@ export default class LoginForm extends Component<Props> {
             name="email"
             id="email"
             placeholder="example@example.com"
-            value={email}
+            value={data.email}
             onChange={this.onChange}
           />
-          {errors.email && <InlineError text={errors.email}/>}
+          {errors.email && <InlineError text={errors.email} />}
         </Form.Field>
         <Form.Field error={Boolean(errors.password)}>
           <label htmlFor="password">Password</label>
@@ -60,10 +68,10 @@ export default class LoginForm extends Component<Props> {
             name="password"
             id="password"
             placeholder="secure"
-            value={password}
+            value={data.password}
             onChange={this.onChange}
           />
-          {errors.password && <InlineError text={errors.password}/>}
+          {errors.password && <InlineError text={errors.password} />}
         </Form.Field>
         <Button primary>Login</Button>
       </Form>
